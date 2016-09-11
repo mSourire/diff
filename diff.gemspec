f07f4d0c51547b9b4befde6e31a01c21014817df
Pavel Gorohovsky <nolan5@yandex.ru>
2016-09-12 00:23:38 +0300

    Added line comparison without line breaks

--- diff.gemspec
+++ diff.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'diff'
-  s.version     = '1.0.0'
-  s.date        = '2016-09-09'
+  s.version     = '1.0.1'
+  s.date        = '2016-09-11'
   s.summary     = "Diff"
   s.description = "A simple diff gem"
   s.authors     = ["Pavel Gorohovsky"]
--- lib/diff.rb
+++ lib/diff.rb
@@ -27,12 +27,21 @@ class Diff
     @result << [line_number, LINE_STATUSES[line_status], line_content]
   end
 
+  def check_equal_lines_endings line1, line2
+    l1_size, l2_size = line1.size, line2.size
+    if l1_size > l2_size
+      add_line(' ', :deleted, '')
+    elsif l2_size > l1_size
+      add_line(' ', :new, '')
+    end
+  end
+
   def construct
     @result = []
     @first.each_with_index do |l1, l1_id|
       found = false
       @second.each_with_index do |l2, l2_id|
-        if found = l2 == l1
+        if found = l2.chomp == l1.chomp
           if new_lines_before = l2_id != 0
             new_lines = @second.shift(l2_id)
             new_lines.each do |l|
@@ -40,6 +49,7 @@ class Diff
             end
           end
           add_line(l1_id + 1, :not_changed, @second.shift)
+          check_equal_lines_endings(l1, l2)
           break
         end
       end
